a542a9be3eaf912cf728f0a5181404d778b628c1
denchik <andrey2008@inbox.ru>
2022-12-25 23:27:39 +0300

    DZ_9

---
+++ DZ_9/dz_9.1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// Задайте значение N. Напишите программу, которая выведет все четные натуральные числа в промежутке от N до 1.
+
+
+void Numbers(int m, int n)
+{
+    if (m > n) return;
+    if (m % 2 == 0)
+    {
+        Console.Write($" {m} ");
+        Numbers(m + 2, n);
+    }
+    else
+    {
+        m++;
+        Console.Write($" {m} ");
+        Numbers(m + 2, n);
+    }
+}
+Console.WriteLine("Введите значение числа m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение числа n: ");
+int n = int.Parse(Console.ReadLine());
+Numbers(m, n);
